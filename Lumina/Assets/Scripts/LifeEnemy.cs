e776c2821ccaad590ea92badb2b64a5e565b255b
batefraco <paragame1001@gmail.com>
2024-11-08 15:39:33 -0300

    boss arrumada
    falta animações dos inimigos

--- Lumina/Assets/Scripts/LifeEnemy.cs
+++ Lumina/Assets/Scripts/LifeEnemy.cs
@@ -32,11 +32,12 @@ public class LifeEnemy : MonoBehaviour
             if(Life <= 0)
             {
                 Destroy(gameObject);
+                if(destroyer != null)
+                {
+                    Destroy(destroyer);
+                }
+
             }
-        }
-        if(destroyer != null)
-        {
-            Destroy(destroyer);
         }
     }
     
